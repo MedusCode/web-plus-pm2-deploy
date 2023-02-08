73cabde60758ee26d4e1ab1ff97c1f50159650b2
MedusCode <87340589+MedusCode@users.noreply.github.com>
2023-02-08 21:47:50 +0300

    fix: ecosystem.config.js

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -20,7 +20,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'cd ~/mesto-backend/current/backend/ && sudo npm i && npm run build',
+      'post-deploy': 'cd ~/mesto-backend/source/backend/ && sudo npm i && npm run build',
     },
   },
 };
